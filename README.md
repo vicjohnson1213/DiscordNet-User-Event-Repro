cc101f137e1e2f5ec72cb41f428b2c402fa2d804
Victor Johnson <vicjohnson1213@gmail.com>
2020-11-12 00:50:32 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Discord.Net User Event Reproduction
+
+This is a reproduction of an issue I've run into where a few user related events aren't triggering. Many events seem to be firing just fine though and I've triple checked that the bots I'm using have Administrator rights in the servers I've tested in.
+
+Versions I've tried:
+
+- 2.2.0
+- 2.3.0-dev-20200801.4
+- 2.3.0-dev-20201109.3
+
+The events that aren't working:
+
+- `client.UserJoined`
+- `client.UserLeft`
+- `client.UserChanged`
+- `client.GuildMemberUpdated`
+
+The events that work fine:
+
+- `client.ChannelCreated`
+- `client.ChannelDestroyed`
+- `client.MessageReceived`
---
+++ TestDiscordBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+
+namespace TestDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            var client = new DiscordSocketClient();
+
+            client.Log += Log;
+            client.UserJoined += OnUserJoin;    // Doesn't work
+            client.UserUpdated += OnUserUpdate;    // Doesn't work
+            client.ChannelCreated += OnChannelCreate;  // Works fine
+
+            await client.LoginAsync(TokenType.Bot, "...");
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+        
+        public async Task OnUserJoin(SocketGuildUser user)
+        {
+            Console.WriteLine(user.ToString());
+        }
+        public async Task OnUserUpdate(SocketUser u1, SocketUser u2)
+        {
+            Console.WriteLine(u1.ToString());
+            Console.WriteLine(u2.ToString());
+        }
+
+        public async Task OnChannelCreate(SocketChannel channel)
+        {
+            Console.WriteLine(channel.ToString());
+        }
+
+        public Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
